ec9b981affca62800fd21ef03c4ac79728df1ec4
AndreNTeixeira <andrenogueirab@gmail.com>
2025-04-10 18:00:07 -0300

    iniciar com random feito

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,18 @@
 import Base.Pessoa;
 import Logica.FiladeEspera;
+import Simulacao.Simulador;
+
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
-        FiladeEspera fila = new FiladeEspera();
+        Simulador s = new Simulador();
+        s.iniciar();
 
-        // Adicionando pessoas
-        fila.adicionar(1, 0, 5, 10, false, false);
-        fila.adicionar(2, 3, 7, 15, true, false);
-        fila.adicionar(6, 2, 0, 20, false, true);
-        fila.adicionar(4, 1, 4, 25, true, true);
-        fila.adicionar(3, 1, 4, 25, true, true);
-        fila.adicionar(7, 1, 4, 25, true, true);
+        //fila.retirar(1);
 
-        fila.imprimir();
-
-        fila.retirar(1);
-
-        fila.imprimir();
+        //fila.imprimir();
 
 
     }
--- src/Simulacao/Simulador.java
+++ src/Simulacao/Simulador.java
@@ -1,25 +1,46 @@
 package Simulacao;
 
 import Base.Predio;
+import Logica.FiladeEspera;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Simulador {
 
     Scanner teclado = new Scanner(System.in);
-
+    FiladeEspera fila;
+    int andares;
     public void iniciar(){
         System.out.println("Iniciando a Simulação");
 
         System.out.print("Digite a quantidade de andares: ");
-        int andares = teclado.nextInt();
+        this.andares = teclado.nextInt();
         System.out.print("Digite a quantidade de elevadores: ");
         int elevadores = teclado.nextInt();
-
+        System.out.print("Digite a quantidade de pesoas: ");
+        int pessoas = teclado.nextInt();
+        this.fila = gerarPessoal(pessoas);
         Predio v1 = new Predio(andares, elevadores);
-        v1.imprimirPredio();
-    }
 
+        v1.imprimirPredio();
+        this.fila.imprimir();
+    }
+    private FiladeEspera gerarPessoal(int pessoas){
+        Random random = new Random();
+        if (pessoas == 0) {
+
+            pessoas = random.nextInt(20);
+        }
+        FiladeEspera fila = new FiladeEspera();
+
+
+        for (int i = 0; i < pessoas; i++) {
+            fila.adicionar(i, random.nextInt(this.andares), 0, random.nextInt(50), false, false);
+        }
+
+        return fila;
+    }
     public void pausar(){
 
     }
